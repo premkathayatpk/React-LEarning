bede6008c216c8f37389a449406aab0c762f707f
Prem Kathayat <132916774+premkathayatpk@users.noreply.github.com>
2024-03-02 18:28:59 +0545

    add todo app

---
+++ Project/to-do-app/src/AddTodo.jsx
@@ -0,0 +1,22 @@
+function AddTodo() {
+  return (
+    <>
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-6">
+            <input type="text" name="" id="" placeholder="Enter todo here" />
+          </div>
+          <div class="col-4">
+            <input type="date" name="" id="" />
+          </div>
+          <div class="col-2">
+            <button type="button" class="btn btn-success">
+              Add
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+export default AddTodo;
---
+++ Project/to-do-app/src/components/appName.jsx
@@ -0,0 +1,8 @@
+function AppName() {
+  return (
+    <>
+      <h1>ToDo React App</h1>
+    </>
+  );
+}
+export default AppName;
