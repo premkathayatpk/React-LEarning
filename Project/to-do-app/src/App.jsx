13d66e470d279d4945655a30ff70689a353aeb8e
Prem Kathayat <132916774+premkathayatpk@users.noreply.github.com>
2024-03-21 17:52:06 +0545

    addtodoitem

--- Project/to-do-app/src/App.jsx
+++ Project/to-do-app/src/App.jsx
@@ -1,4 +1,4 @@
-import AddTodo from "./AddTodo";
+import AddTodo from "./components/AddTodo";
 import AppName from "./components/appName";
 
 function App() {
@@ -7,17 +7,8 @@ function App() {
       <center className="todo-container">
         <AppName />
         <AddTodo />
+        <Todoitem />
         <div class="container text-center">
-          <div class="row">
-            <div class="col-6">Buy Milk</div>
-            <div class="col-4">4/10/2023</div>
-            <div class="col-2">
-              <button type="button" class="btn btn-danger">
-                Delete
-              </button>
-            </div>
-          </div>
-
           <div class="row">
             <div class="col-6">Go to College</div>
             <div class="col-4">4/10/2023</div>
